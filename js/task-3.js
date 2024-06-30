7ac747896a4632c9f86a1a08eb04e12e56d26abc
Anna Kroka <avkroka.broker@gmail.com>
2024-06-30 13:01:46 +0300

    create task1

--- js/task-3.js
+++ js/task-3.js
@@ -27,19 +27,21 @@
 
 //-------------- TASK 3-3--------------------
 
-function filterArray(numbers, value) {
-    let arrayNumber = [];
-    for (let i = 0; i < numbers.length; i++) {
-        if (numbers[i] > value) {
-            arrayNumber.push(numbers[i]);
-        } 
+// function filterArray(numbers, value) {
+//     let arrayNumber = [];
+//     for (let i = 0; i < numbers.length; i++) {
+//         if (numbers[i] > value) {
+//             arrayNumber.push(numbers[i]);
+//         }
         
-    }
-    return arrayNumber;
-}
+//     }
+//     return arrayNumber;
+// }
 
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+// console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+// console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+// console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+// console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+// console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+
+//-------------- TASK 3-4--------------------
